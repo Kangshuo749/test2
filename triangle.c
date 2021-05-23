8b6ecbdf8fa2e39f247961a8b21bdef7be95ca2d
Kangshuo <814574132@qq.com>
2021-05-23 21:13:51 +0800

    杨辉三角

--- triangle.c
+++ triangle.c
@@ -6,28 +6,28 @@
 //……
 //arr[i][0]恒等于1，当i与j相等的时候arr[i][j]恒等于1
 //arr[i][j]=arr[i-1][j]+arr[i-1][j-1]
-#include<stdio.h>
-#include<stdlib.h>
-int main() {
-	int i = 0;
-	int j = 0;
-	int arr[10][10] = { 0 };
-	for (i = 0; i < 10; i++) {
-		for (j = 0; j < 10; j++) {
-			if (i == j || j==0) {
-				arr[i][j] = 1;
-             }
-			else {
-				arr[i][j] =arr[i - 1][j] + arr[i - 1][j - 1];
-			}
-		}
-	}
-	for (i = 0; i < 10; i++) {
-		for (j = 0; j <=i; j++) {
-			printf("%d ", arr[i][j]);
-		}
-		printf("\n");
-	}
-	system("pause");
-	return 0;
-}
+//#include<stdio.h>
+//#include<stdlib.h>
+//int main() {
+//	int i = 0;
+//	int j = 0;
+//	int arr[10][10] = { 0 };
+//	for (i = 0; i < 10; i++) {
+//		for (j = 0; j < 10; j++) {
+//			if (i == j || j==0) {
+//				arr[i][j] = 1;
+//             }
+//			else {
+//				arr[i][j] =arr[i - 1][j] + arr[i - 1][j - 1];
+//			}
+//		}
+//	}
+//	for (i = 0; i < 10; i++) {
+//		for (j = 0; j <=i; j++) {
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
